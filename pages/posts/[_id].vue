fc8ea38e1b89be4b0ec53bf7944fa93a838d0b44
Antonii <58983215+antoniiAdamovych@users.noreply.github.com>
2023-06-02 14:23:37 +0200

    Update [_id].vue

--- pages/posts/[_id].vue
+++ pages/posts/[_id].vue
@@ -4,7 +4,7 @@
       {{ data.name }}
     </Title>
     <Meta name="og:image" :content="data.images[0].file.url" />
-    <Meta name="twitter:description" :content="data.meta_description" />
+    <Meta name="description" :content="data.meta_description" />
   </Head>
   <section class="product">
     <div class="container">
